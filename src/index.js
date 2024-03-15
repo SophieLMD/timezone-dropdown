b64e7203f26bb22999e671ddddbc3316ae23a420
SophieLMD <160946529+SophieLMD@users.noreply.github.com>
2024-03-15 06:26:16 +0000

    Created showSelectedCity function to alert city name when selected

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-
+function showSelectedCity(event) {
+  if (event.target.value.length > 0) {
+    alert(event.target.value);
+  }
+}
+let citiesSelect = document.querySelector("#city");
+citiesSelect.addEventListener("change", showSelectedCity);
+showSelectedCity();
