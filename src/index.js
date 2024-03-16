862cf3e28e84581c784045efe91394227afda6f2
SophieLMD <160946529+SophieLMD@users.noreply.github.com>
2024-03-16 14:00:25 +0000

    Installed moment.js library and moment timezone

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-function showSelectedCity(event) {
+function showCityTimezone(event) {
+  let citiesSelect = document.querySelector("#city");
+  citiesSelect.addEventListener("change", showCityTimezone);
   if (event.target.value.length > 0) {
     alert(event.target.value);
   }
 }
-let citiesSelect = document.querySelector("#city");
-citiesSelect.addEventListener("change", showSelectedCity);
-showSelectedCity();
+showCityTimezone();
